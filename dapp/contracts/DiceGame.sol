4672041bbc19fb748da430ecc71444f4b745ffe8
hanzopgp <enzodurandcontact@gmail.com>
2021-03-10 22:29:05 +0100

    comms

--- dapp/contracts/DiceGame.sol
+++ dapp/contracts/DiceGame.sol
@@ -226,7 +226,7 @@ contract DiceGame is Ownable{ //Ownable allows use onlyOwner modifier so we can 
         uint i;
         result = 0;
         for (i = 0; i < b.length; i++) {
-            uint c = uint(uint8(b[i]));
+            uint c = uint(uint8(b[i])); //Conversion trick
             if (c >= 48 && c <= 57) {
                 result = result * 10 + (c - 48);
             }
